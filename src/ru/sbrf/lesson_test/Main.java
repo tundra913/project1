ddc0e84472964e27f29474450969ad54f02ffe67
Denis <denis.for.logs@gmail.com>
2020-08-09 22:41:21 +0300

    Уточнения по коду
    Уточнения по коду

--- src/ru/sbrf/lesson_test/Main.java
+++ src/ru/sbrf/lesson_test/Main.java
@@ -17,25 +17,31 @@ public class Main {
         boolean isCharA = add4('B');                    //задание 7
         System.out.println(isCharA);
 
+        //по условию требуется реализовать ввиде метода
         char isCharNumber = '5';               //задание 8
         System.out.println(Character.isDigit(isCharNumber));
 
+        //по условию требуется реализовать ввиде метода
         for (int bit = 0; bit < 30; bit++) {                //задание 9
             System.out.println(Integer.toBinaryString(bit));
         }
-
+        //по условию требуется реализовать ввиде метода
         System.out.println(Integer.toBinaryString(Integer.MAX_VALUE));
-        System.out.println(Integer.toBinaryString(Integer.MAX_VALUE + 1));      //задание 10
-
+        System.out.println(Integer.toBinaryString(Integer.MAX_VALUE + 1));      //задание 1
+        
+        //по условию требуется реализовать ввиде метода
         char chr = 'a';                                //задание 11
         System.out.println(Integer.valueOf(chr));
-
+        
+        //по условию требуется реализовать ввиде метода
         int num = 123;
         System.out.println(Integer.toString(num));     //задание 12
 
     }
 
     public static byte add(int a, int b) {
+        //просьба пересмотреть условия задания
+        //типы параметров в условии отличаются
         return (byte) (a + b);
     }
 
@@ -44,6 +50,8 @@ public class Main {
     }
 
     public static int add3(int i, int j) {
+        //решение принимается
+        //в качестве дополнительной задачи предлагается подумать над более коротким решением задачи
         if (i < j) {
             return j;
         } else {
@@ -52,6 +60,9 @@ public class Main {
     }
 
     public static boolean add4(char z) {
+        
+        //решение принимается
+        //в качестве дополнительной задачи предлагается подумать над более коротким решением задачи
         if (z == 'A') {
             return true;
         } else {
