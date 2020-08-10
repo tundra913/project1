214ba75b512d0456c19b27cc30717703f9ab6023
tundra913 <68968396+tundra913@users.noreply.github.com>
2020-08-10 12:48:46 +0300

    done

--- src/ru/sbrf/lesson_test/Main.java
+++ src/ru/sbrf/lesson_test/Main.java
@@ -4,71 +4,61 @@ package ru.sbrf.lesson_test;
 public class Main {
     public static void main(String[] args) {
 
-        byte theSum = add(1, 1);
-        System.out.println(theSum);                             //задание 4
+        byte a = 1;
+        short b = 1;
+        int c = 13;
+        long d = 14253356;
+        int g = 548;
+        char f = '6';
 
-        int theMult = add2(13, 14253356);
-        System.out.println(theMult);                            //задание 5
-
-        int getMaxNumber = add3(170, 48);
-        System.out.println(getMaxNumber);                       //задание 6
-
-
-        boolean isCharA = add4('B');                    //задание 7
-        System.out.println(isCharA);
-
-        //по условию требуется реализовать ввиде метода
-        char isCharNumber = '5';               //задание 8
-        System.out.println(Character.isDigit(isCharNumber));
-
-        //по условию требуется реализовать ввиде метода
-        for (int bit = 0; bit < 30; bit++) {                //задание 9
-            System.out.println(Integer.toBinaryString(bit));
-        }
-        //по условию требуется реализовать ввиде метода
-        System.out.println(Integer.toBinaryString(Integer.MAX_VALUE));
-        System.out.println(Integer.toBinaryString(Integer.MAX_VALUE + 1));      //задание 1
-        
-        //по условию требуется реализовать ввиде метода
-        char chr = 'a';                                //задание 11
-        System.out.println(Integer.valueOf(chr));
-        
-        //по условию требуется реализовать ввиде метода
-        int num = 123;
-        System.out.println(Integer.toString(num));     //задание 12
-
+        System.out.println(theSum(a, b));                //задание 4
+        System.out.println(theMult(c, d));               //задание 5
+        System.out.println(getMaxNumber(c, g));         //задание 6
+        System.out.println(isCharA(f));                 //задание 7
+        System.out.println(isCharNumber(f));            //задание 8
+        toBinary();                                     //задание 9
+        mVal();                                         //задание 10
+        System.out.println(charToInt(f));               //задание 11
+        System.out.println(intToChar(g));               //задание 12
     }
 
-    public static byte add(int a, int b) {
-        //просьба пересмотреть условия задания
-        //типы параметров в условии отличаются
+    public static byte theSum(byte a, short b) {
         return (byte) (a + b);
     }
 
-    public static int add2(int c, long d) {
+    public static int theMult(int c, long d) {
         return (int) (c * d);
     }
 
-    public static int add3(int i, int j) {
-        //решение принимается
-        //в качестве дополнительной задачи предлагается подумать над более коротким решением задачи
-        if (i < j) {
-            return j;
-        } else {
-            return i;
+    public static int getMaxNumber(int c, int g) {
+        return c > g ? c : g;
+    }
+
+    public static boolean isCharA(char f) {
+        return f == 'A' ? true : false;
+    }
+
+    public static boolean isCharNumber(char f) {
+        return Character.isDigit(f) ? true : false;
+
+    }
+
+    public static void toBinary() {
+        for (int bit = 0; bit < 30; bit++) {
+            System.out.println(Integer.toBinaryString(bit));
         }
     }
 
-    public static boolean add4(char z) {
-        
-        //решение принимается
-        //в качестве дополнительной задачи предлагается подумать над более коротким решением задачи
-        if (z == 'A') {
-            return true;
-        } else {
-            return false;
-        }
+    public static void mVal() {
+        System.out.println(Integer.toBinaryString(Integer.MAX_VALUE));
+        System.out.println(Integer.toBinaryString(Integer.MAX_VALUE + 1));
     }
 
+    public static int charToInt(char k) {
+        return k;
+    }
 
+    public static char intToChar(int h)  {
+        return (char) h;
+    }
 }
